1779567f6b2e70d35515dcb1efd4b4972bfefb30
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-09-15 18:54:45 +0530

    fix - not used vars warning

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import styles from './Footer.module.css'
 import GitHubIcon from '@material-ui/icons/GitHub'
@@ -6,13 +7,13 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn'
 const Footer = () => {
     return (
       <footer>
-            <a href='https://github.com/foolhardy21' target='_blank'>
+            <a href='https://github.com/foolhardy21' target='_blank' rel="noreferrer">
               <GitHubIcon style={{ color: 'white' }} />
             </a>
-            <a href='https://www.linkedin.com/in/vinay-kumar-279a91167/' target='_blank'>
+            <a href='https://www.linkedin.com/in/vinay-kumar-279a91167/' target='_blank' rel="noreferrer">
               <LinkedInIcon style={{ color: 'white' }} />
             </a>
-            <a href='https://twitter.com/foolhardy_vinay' target='_blank'>
+            <a href='https://twitter.com/foolhardy_vinay' target='_blank' rel="noreferrer">
               <TwitterIcon style={{ color: 'white' }} />
             </a>
       </footer>
