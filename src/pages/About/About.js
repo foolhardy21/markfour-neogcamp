42c0f5ae719abf96aaad5d27ee57c5fb141a3349
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-09-21 16:17:46 +0530

    refactor - projectslist update

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <section className={styles.about_section}>
             <p>Started with HTML, CSS, JavaScript, React in Feb 2021.</p><br />
             <p>Graduated with Computer Science and Engineering in July 2021.</p><br />
-            <p> Joined a service based industry as a Software Engineer in July 2021.</p><br />
+            <p> Joined a service based company as a Software Engineer in July 2021.</p><br />
             <p className={styles.highlight}>Soon to be a Web Developer.</p>
         </section>
     )
--- src/pages/Project/projectdata.js
+++ src/pages/Project/projectdata.js
@@ -13,6 +13,64 @@ const projects = [
         url: 'https://replit.com/@foolhardy21/mktwo?v=1',
         source: 'https://replit.com/@foolhardy21/mktwo?v=1',
     },
+    {
+        name: 'Minion Speak',
+        description: 'A translation app using an API',
+        date: new Date('2021-9-16'),
+        url: 'https://zen-newton-3bbb1d.netlify.app/',
+        source: 'https://github.com/foolhardy21/marksix-neogcamp',
+    },
+    {
+        name: 'Know your emotions',
+        description: 'An emoji translation app',
+        date: new Date('2021-9-17'),
+        url: 'https://serene-hodgkin-2f513c.netlify.app/',
+        source: 'https://github.com/foolhardy21/markeight-neogcamp',
+    },
+    {
+        name: 'GoodMovies',
+        description: 'A movie recommendation app',
+        date: new Date('2021-9-18'),
+        url: 'https://loving-lamarr-4fa578.netlify.app/',
+        source: 'https://github.com/foolhardy21/marknine-neogcamp',
+    },
+    {
+        name: 'Cash Register',
+        description: 'An app that calculates the return amount and denominations of notes',
+        date: new Date('2021-9-19'),
+        url: 'https://awesome-jackson-edf166.netlify.app/',
+        source: 'https://github.com/foolhardy21/markten-neogcamp',
+    },
+    {
+        name: 'Lucky birthdate',
+        description: 'An app to check if the birthdate is lucky or not',
+        date: new Date('2021-9-19'),
+        url: 'https://sleepy-tesla-96d64f.netlify.app/',
+        source: 'https://github.com/foolhardy21/markeleven-neogcamp',
+    },
+    {
+        name: 'Fun with triangles',
+        description: 'An app about triangles',
+        date: new Date('2021-9-20'),
+        url: 'https://hungry-almeida-10047c.netlify.app/',
+        source: 'https://github.com/foolhardy21/marktwelve-neogcamp',
+    },
+    {
+        name: 'Palindrome birthdate',
+        description: 'An app that checks whether the birthdate is palindrome',
+        date: new Date('2021-9-20'),
+        url: 'https://dazzling-easley-5a1ce4.netlify.app/',
+        source: 'https://github.com/foolhardy21/markthirteen-neogcamp',
+    },
+    {
+        name: 'Profit or Loss',
+        description: 'An app that quantifies profit or loss',
+        date: new Date('2021-9-21'),
+        url: 'https://mystifying-wilson-9b4849.netlify.app/',
+        source: 'https://github.com/foolhardy21/markfourteen-neogcamp',
+    },
+    
+    
 ]
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
