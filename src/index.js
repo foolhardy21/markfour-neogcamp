c89acf8855af0579c42aad6b76821573cfdaeb7e
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-11-21 23:19:14 +0530

    refactor - react-snap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from "react-dom";
 import App from './App';
 import {ThemeProvider} from './context'
 
-ReactDOM.render(
-<ThemeProvider>
-  <App />
-</ThemeProvider>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  console.log('h')
+  hydrate(<ThemeProvider><App /></ThemeProvider>, rootElement);
+} else {
+  console.log('r')
+  render(<ThemeProvider><App /></ThemeProvider>, rootElement);
+}
