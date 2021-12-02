3e43e901df17482921450ab5f2a420be82c20d83
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-12-02 18:05:05 +0530

    fix - og:image

--- src/pages/BlogList/listing_page_data.js
+++ src/pages/BlogList/listing_page_data.js
@@ -8,20 +8,20 @@ const blogs_list = [
         
     },
     {
-        id: 1,
-        title: 'prototypal inheritance',
-        url: 'prototypal-inheritance',
-        date: new Date('2021-09-23'),
-        description: 'Before we start with this concept, lets understand the meaning of prototype and inheritance.'
-        
-    },
-    {
         id: 0,
         title: 'factory functions',
         url: 'factory-functions',
         date: new Date('2021-09-26'),
         description: 'Factory functions, some say is a better design pattern than constructors.',
             
+    },
+    {
+        id: 1,
+        title: 'prototypal inheritance',
+        url: 'prototypal-inheritance',
+        date: new Date('2021-09-23'),
+        description: 'Before we start with this concept, lets understand the meaning of prototype and inheritance.'
+        
     },
 ]
 
