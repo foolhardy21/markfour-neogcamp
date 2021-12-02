91dca85c5c63c35e23879a14e08abdc2a4abc685
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-12-02 15:55:24 +0530

    feat - blog link preview

--- src/pages/Blogs/Blog3/Blog3.js
+++ src/pages/Blogs/Blog3/Blog3.js
@@ -16,14 +16,14 @@ const Blog3 = () => {
        
        <section className={`${theme==='light' ? styles.blog_section_light : styles.blog_section_dark}`}>
            <Helmet>
-                <meta property="og:image" content="" />
+                <meta property="og:image" content="https://raw.githubusercontent.com/foolhardy21/markfour-neogcamp/main/src/pages/Blogs/assets/blogs_og.png" />
                 <meta property="og:title" content="Portfolio Update" />
                 <meta property="og:description" content="This blog is about all the things I learnt while updating my portfolio site and most of it has to do with client side rendering." />
                 <meta name="twitter:card" content="summary" />
                 <meta name="twitter:site" content="@foolhardy_vinay" />
                 <meta name="twitter:title" content="Portfolio Update" />
                 <meta name="twitter:description" content="This blog is about all the things I learnt while updating my portfolio site and most of it has to do with client side rendering." />
-                <meta name="twitter:image" content="" />        
+                <meta name="twitter:image" content="https://raw.githubusercontent.com/foolhardy21/markfour-neogcamp/main/src/pages/Blogs/assets/blogs_og.png" />        
            </Helmet>
            <article className={`${theme==='light' ? styles.blog_article_light : styles.blog_article_dark}`}>
             <ReactMarkdown>
